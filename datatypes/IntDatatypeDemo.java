adc5de04d3c1f925c408e4292e82ae819a67c222
eunjaelim <imeunjae361@gmail.com>
2023-03-02 02:25:07 +0900

    자바기본개념

---
+++ datatypes/IntDatatypeDemo.java
@@ -0,0 +1,14 @@
+package org.opentutorials.javatutorials.datatypes;
+
+public class IntDatatypeDemo {
+
+	public static void main(String[] args) {
+		byte a;
+		a = 127;
+		//a = 128;
+		//byte population = 50000000;
+		
+
+	}
+
+}
---
+++ variables/IntergerDemo.java
@@ -0,0 +1,14 @@
+package org.opentutorials.javatutorials.variables;
+
+public class IntergerDemo {
+
+	public static void main(String[] args) {
+		int a; // 변수의 선언
+		a=1; // 할당
+		System.out.println(a+1);
+		a=2;
+		System.out.println(a+1);
+
+	}
+
+}
